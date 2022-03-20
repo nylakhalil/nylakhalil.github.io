05eac05836687e0c94d728e17d7bec2c8495031f
Nyla Khalil <nylakhalil@users.noreply.github.com>
2022-03-20 01:44:54 -0400

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders app home", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameAnchorNode = screen.getByText(/nyla khalil/i, {selector: 'h1'});
+  const aboutAnchorNode = screen.getByText(/about/i);
+  const developAnchorNode = screen.getByText(/develop/i);
+  const photographyAnchorNode = screen.getByText(/photography/i);
+
+  expect(nameAnchorNode).toBeInTheDocument();
+  expect(aboutAnchorNode).toBeInTheDocument();
+  expect(developAnchorNode).toBeInTheDocument();
+  expect(photographyAnchorNode).toBeInTheDocument();
 });
